243680147cdec0a972e3d67c875b2bcec5842220
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-21 09:50:42 -0600

    WIP: hamburger menu built, need to add functionality

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -10,7 +10,6 @@ class Carousel{
         this.leftButton.addEventListener('click', () => this.scrollLeft())
         this.rightButton.addEventListener('click', () => this.scrollRight())
         this.content = document.querySelector('.content')
-
     }
     scrollRight(){
         TweenMax.to(this.content, 0.5, {opacity:0})
@@ -19,7 +18,7 @@ class Carousel{
         TweenMax.to(this.images[this.currentIndex], 0, {display:'none', delay:1})
         if (this.images.length-1 === this.currentIndex){this.currentIndex = 0}
         else {this.currentIndex += 1}
-        setTimeout(this.updateContent(), 500)
+        setTimeout(() => this.updateContent(), 500)
         TweenMax.to(this.images[this.currentIndex], 0, {display:'block',opacity:0, delay:1})
         TweenMax.to(this.images[this.currentIndex], 1, {opacity:1, delay:1})
         TweenMax.to(this.content, 0, {display:'block',opacity:0, delay:1.5})
@@ -32,7 +31,7 @@ class Carousel{
         TweenMax.to(this.images[this.currentIndex], 0, {display:'none', delay:1})
         if (this.currentIndex === 0){this.currentIndex = this.images.length-1}
         else {this.currentIndex -= 1}
-        setTimeout(this.updateContent(), 500)
+        setTimeout(() => this.updateContent(), 500)
         TweenMax.to(this.images[this.currentIndex], 0, {display:'block',opacity:0, delay:1})
         TweenMax.to(this.images[this.currentIndex], 1, {opacity:1, delay:1})
         TweenMax.to(this.content, 0, {display:'block',opacity:0, delay:1.5})
@@ -44,7 +43,7 @@ class Carousel{
             this.content.style.left = '15%'
         }
         else if (this.currentIndex === 1){
-            this.content.style.top = '70%'
+            this.content.style.top = '65%'
             this.content.style.left = '15%'
         }
     }
